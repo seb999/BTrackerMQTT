d9d55750e30e33c977238eb62268a9f7d1c51b64
Sebastien <sebastien@MacBook-Pro-de-Sebastien.local>
2019-10-29 22:24:39 +0100

    added new device

--- App.js
+++ App.js
@@ -1,21 +1,24 @@
 //The Thing Network Node.js SDK
+
+//Create a MQTT client connected to our thing network application
 const appId = '628799427265176';
-const accessKey = 'ttn-account-v2.42To_tkueS4f9vn9n8Iml6XleeN0-sjeAEu5eEX5cpE';
+const accessKey = 'ttn-account-v2.llxrO7pihjYSaatgK--0dEYPb0yIxUVaMh5Isqq95Xo';
 const url = 'eu.thethings.network:1883';
 const port = process.env.PORT || 4001;
-
 const ttn = require('ttn');
+var ttnClient = new ttn.DataClient(appId, accessKey, url);
+
+//Create a http server using express
 const express = require("express");
 const http = require("http");
 const indexPage = require("./routes/index");
-
-var client = new ttn.DataClient(appId, accessKey, url);
-
 const app = express();
 app.use(indexPage);
 const server = http.createServer(app);
+server.listen(port, () => console.log(`Listening on port ${port}`));
 
-const io = require("socket.io")(server, {
+//Create socker.io client that will use this server
+const socket = require("socket.io")(server, {
     handlePreflightRequest: (req, res) => {
         const headers = {
             "Access-Control-Allow-Headers": "Content-Type, Authorization",
@@ -27,24 +30,55 @@ const io = require("socket.io")(server, {
     }
 });
 
-io.on("connection", socket => {
+//Open connection HERE!!
+socket.on("connection", socket => {
 
-    //To send only device ID
-    // client.on('uplink', function (msg)
-    // {
-    //     console.log('Received message', msg);
-    //     socket.emit("FromLoraTracker", msg);
-    // });
-
-    client.on("uplink", function (devID, payload) {
+    ttnClient.on("uplink", function (devID, payload) {
        console.log("Received uplink from : ", devID)
        obj = JSON.stringify(payload);
        obj2 = JSON.parse(obj);
        socket.emit("FromLoraTracker", obj2.hardware_serial);
     })
+
+    socket.on('addNewDevice', function(devEUI){ 
+        console.log("Received downlink from BTracker" , devEUI);
+        regiterNewDevice();
+     });
 });
 
-server.listen(port, () => console.log(`Listening on port ${port}`));
+//For debbuging
+// ttnClient.on("uplink", function (devID, payload) {
+//     console.log("Received uplink from : ", devID)
+//     obj = JSON.stringify(payload);
+//     obj2 = JSON.parse(obj);
+//  })
+
+//Asynch method to register a new device
+const regiterNewDevice = async function (EUI) {
+    const application = await ttn.application(appId, accessKey)
+
+   const euis = await application.getEUIs();
+   const devices = await application.devices();
+   const newDeviceId = "test" + (devices.length + 1).toString();
+    
+   // register a new device
+    await application.registerDevice(newDeviceId, {
+      description: "Description",
+      appEui: euis[0],
+      devEui: "9988776655443324",
+      nwkSKey: ttn.key(16),
+      appSKey: ttn.key(16),
+      appKey: ttn.key(16),
+      
+    })
+}
+
+regiterNewDevice("abc").catch(function (err) {
+    console.error(err)
+    process.exit(1)
+  })
+
+
 
 
 
